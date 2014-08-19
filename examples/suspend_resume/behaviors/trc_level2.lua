7ee75de072074fb92bcfd1150c899f31d0e8b222
root <agusct@gmail.com>
2014-08-19 19:38:56 -0300

    suspend_remove example now also adds a behavior on running time.

--- examples/suspend_resume/behaviors/trc_level2.lua
+++ examples/suspend_resume/behaviors/trc_level2.lua
@@ -1,6 +1,7 @@
 -- /// trc_level2 ///
 
 local toroco = require 'toroco.toroco'
+local device = toroco.device
 local behavior = toroco.behavior
 
 -- /// Handler functions ///
@@ -22,9 +23,37 @@ end
 
 local handler_remove = function (event, value) 
     if value then
-        print ('level1 removed') 
+        print ('level2 removed') 
 
-        toroco.remove_behavior (behavior.trc_level1)
+        toroco.remove_behavior (behavior.trc_level2)
+
+        print ('new level2 added') 
+
+        local trc_level2 = {
+            name = 'trc_level2';
+
+            output_events = { }; 
+            
+            input_sources = {    
+                trigger1 = device.mice.rightbutton;
+            };
+
+            input_handlers = {
+                trigger1 = function (event, value)
+                    if value then
+                        print ('inhibition started')        
+                        toroco.inhibit (device.mice.leftbutton, 2.5)
+                        --toroco.suppress (device.mice.leftbutton, behavior.trc_sender, 2.5)
+
+                    else
+                        print ('inhibition released')   
+                        toroco.release_inhibition (device.mice.leftbutton)
+                        --toroco.release_suppression (device.mice.leftbutton, behavior.trc_sender)
+                    end
+                end
+            };
+        }
+        toroco.add_behavior (trc_level2)
     end
 end
 
@@ -32,7 +61,7 @@ return {
     output_events = { }; 
     
     input_handlers = {
-        trigger1 = handler_suspend;
-        --trigger1 = handler_remove;
+        --trigger1 = handler_suspend;
+        trigger1 = handler_remove;
     } 
 }
