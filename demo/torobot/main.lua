d0bbf01f512a1de7f9091e708581fb78c2c51a39
Agustin <agusct@gmail.com>
2014-10-19 19:45:14 -0200

    Torobot: New wander behavior.

--- demo/torobot/main.lua
+++ demo/torobot/main.lua
@@ -8,13 +8,14 @@ local behavior = toroco.behavior
 
 -- initialize behaviors
 
-toroco.load_behavior (behavior.move_forward, 'behaviors/trc_move_forward', {motors_values = {20, 20}})
+--toroco.load_behavior (behavior.move_forward, 'behaviors/trc_move_forward', {motors_values = {20, 20}})
 toroco.load_behavior (behavior.turn_left, 'behaviors/trc_turn', {motors_values = {5, 20}})
 toroco.load_behavior (behavior.turn_right, 'behaviors/trc_turn', {motors_values = {20, 5}})
 toroco.load_behavior (behavior.red_follower, 'behaviors/red_follower',
     {motors_lx = {15, 50}, motors_l = {30, 50},
     motors_f = {50, 50},
     motors_r = {50, 30}, motors_rx = {50, 15}})
+toroco.load_behavior (behavior.wander, 'behaviors/trc_wander')
 
 -- initialize inputs
 ---[[
@@ -28,7 +29,8 @@ toroco.set_inputs (behavior.turn_right, {
 
 toroco.set_inputs (device.servo_motors, {
     setvel2mtr = {
-        behavior.move_forward.motors_setvel,
+        --behavior.move_forward.motors_setvel,
+        behavior.wander.motors_setvel,
         behavior.turn_left.motors_setvel,
         behavior.turn_right.motors_setvel,
         behavior.red_follower.motors_setvel
